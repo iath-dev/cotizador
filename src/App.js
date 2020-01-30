fda16ad0ed64ec2de3e15569ade9eed8d4cc41b0
SOLUTRAFFICDEVELOPERALPHA <solutrafficdev001@gmail.com>
2020-01-30 08:49:50 -0500

    88. Añadiendo formulario

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header';
+import styled from '@emotion/styled';
+import Form from './components/form';
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const FormContainer = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Header title="Cotizador" />
+      <FormContainer>
+        <Form />
+      </FormContainer>
+    </Container>
+    );
 }
 
 export default App;
